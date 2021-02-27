2aab752ab090a6a96eed31991236593cbf23aef4
Lindsay Esterman <lindsayesterman@gmail.com>
2021-02-27 15:03:24 -0500

    config db setup

--- postgrator-config.js
+++ postgrator-config.js
@@ -1,9 +1,11 @@
 require('dotenv').config();
+console.log(process.env.DATABASE_URL);
+const config = require('./src/config')
 
 module.exports = {
     "migrationsDirectory": "migrations",
     "driver": "pg",
     "connectionString": (process.env.NODE_ENV === 'test')
-    ? process.env.TEST_DATABASE_URL
-    : process.env.DATABASE_URL,
+    ? config.TEST_DATABASE_URL
+    : config.DATABASE_URL,
 }
