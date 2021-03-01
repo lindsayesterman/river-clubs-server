7de46004270cf1f7409f8426cc5c0619a4eb5b79
Lindsay Esterman <lindsayesterman@gmail.com>
2021-03-01 17:38:29 -0500

    change config to env

--- postgrator-config.js
+++ postgrator-config.js
@@ -6,6 +6,6 @@ module.exports = {
     "migrationsDirectory": "migrations",
     "driver": "pg",
     "connectionString": (process.env.NODE_ENV === 'test')
-    ? config.TEST_DATABASE_URL
-    : config.DATABASE_URL,
+    ? process.env.TEST_DATABASE_URL
+    : process.env.DATABASE_URL,
 }
