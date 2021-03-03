2b8d819f84c2540468d0d4106a7a316ca76030c1
Lindsay Esterman <lindsayesterman@gmail.com>
2021-03-03 12:30:27 -0500

    connection string ssl

--- postgrator-config.js
+++ postgrator-config.js
@@ -1,10 +1,14 @@
-require('dotenv').config();
+require("dotenv").config();
 console.log(process.env.DATABASE_URL);
 
 module.exports = {
-    "migrationsDirectory": "migrations",
-    "driver": "pg",
-    "connectionString": (process.env.NODE_ENV === 'test')
-    ? process.env.TEST_DATABASE_URL
-    : process.env.DATABASE_URL,
-}
+  migrationsDirectory: "migrations",
+  driver: "pg",
+  connectionString:
+    process.env.NODE_ENV === "test"
+      ? process.env.TEST_DATABASE_URL
+      : process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+};
