7961118c3cb6dc9093b61daabb33286cda12cf4b
Lindsay Esterman <lindsayesterman@gmail.com>
2021-03-04 20:41:20 -0500

    working migrations

--- src/server.js
+++ src/server.js
@@ -4,8 +4,7 @@ const { PORT, DATABASE_URL } = require('./config')
 
 const db = knex({
   client: 'pg',
-  connection: DATABASE_URL,
-  connection: { ssl: true }
+  connection: DATABASE_URL
 })
 
 app.set('db', db)
